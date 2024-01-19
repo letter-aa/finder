be66d341af149c47011c8a1f00beda5e789de9f2
a <102840087+letter-aa@users.noreply.github.com>
2024-01-19 11:01:10 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <conio.h>
 #include <string>
+#include <thread>
 #define ANY_ARROW 224
 #define LEFT_ARROW 75
 #define RIGHT_ARROW 77
@@ -43,19 +44,16 @@ void getcontentofdir(std::vector<string>& out, vector<bool> & isdir, const strin
     } while (FindNextFile(handle, &wfd));
     FindClose(handle);
 } // GetFilesInDirectory
-
 int woc() {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
     return csbi.dwSize.X;
 }
-
 int hoc() {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
     return csbi.dwSize.Y;
 }
-
 void clear() { //may or may have not found this on stack overflow 😍😍
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -73,10 +71,9 @@ void clear() { //may or may have not found this on stack overflow 😍😍
     FillConsoleOutputAttribute(console, csbi.wAttributes, uhoh, b, &dw);
     SetConsoleCursorPosition(console, b);
 }
-
-void exfunc1(vector<string> nuhuh, int inc, string path) {
+void exfunc1(vector<string> nuhuh, int inc, string path, string start_path) {
     vector<string> yuhuh;
-    if (path != "C:\\") {
+    if (path != start_path) {
         string expath = path;
         vector<bool> exv;
         expath = expath.substr(0, expath.find_last_of("\\", expath.find_last_of("\\") - 1) + 1);
@@ -91,20 +88,23 @@ void exfunc1(vector<string> nuhuh, int inc, string path) {
     }
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0,(short)inc});
 }
-
-string finder() {
+string finder(string path) {
+    string start_path = path;
     hidecursor(true);
     int inc = 0;
-    string path = "C:\\";
     vector<string> nuhuh;
     vector<bool> dun;
     vector<int> prevInc;
     getcontentofdir(nuhuh, dun, path);
-    exfunc1(nuhuh, inc, path);
+    exfunc1(nuhuh, inc, path, start_path);
+    if (nuhuh.empty() == true) {
+        return "finder(): invalid path! please try again with a different path.";
+    }
     int i = 0;
     int j = 0;
     bool dirIsEmpty = false;
     bool ex1 = false;
+    bool ex2 = false;
     while (i = _getch()) {
         switch (i) {
         case ANY_ARROW:
@@ -114,7 +114,7 @@ string finder() {
                 if (dirIsEmpty == false) {
                     if (inc > 0) {
                         inc--;
-                        exfunc1(nuhuh, inc, path);
+                        exfunc1(nuhuh, inc, path, start_path);
                     }
                 }
                 break;
@@ -122,19 +122,20 @@ string finder() {
                 if (dirIsEmpty == false) {
                     if (inc < nuhuh.size() - 1) {
                         inc++;
-                        exfunc1(nuhuh, inc, path);
+                        exfunc1(nuhuh, inc, path, start_path);
                     }
                 }
                 break;
             case LEFT_ARROW:
-                if (path != "C:\\") {
+                if (path != start_path) { // "C:\\"
+                    ex2 = ex1;
                     ex1 = false;
                     dirIsEmpty = false;
                     path = path.substr(0, path.find_last_of("\\", path.find_last_of("\\") - 1) + 1);
                     getcontentofdir(nuhuh, dun, path);
                     inc = prevInc.back();
                     prevInc.pop_back();
-                    exfunc1(nuhuh, inc, path);
+                    exfunc1(nuhuh, inc, path, start_path);
                 }
                 break;
             case RIGHT_ARROW:
@@ -147,7 +148,7 @@ string finder() {
                             dirIsEmpty = true;
                         }
                         inc = 0;
-                        exfunc1(nuhuh, inc, path);
+                        exfunc1(nuhuh, inc, path, start_path);
                     }
                 }
                 break;
@@ -176,13 +177,20 @@ string finder() {
             if (ex1 == false) {
                 cout << "this folder is empty.";
                 ex1 = true;
-                Sleep(2500);
-                cout << "\n\nclick on the left arrow to go back.";
+                ex2 = false;
+                thread thread1([&ex1, &ex2]() {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+                    if (ex1 == true && ex2 == false) {
+                        cout << "\n\nclick on the left arrow to go back.";
+                    }
+                    ex2 = false;
+                    ex1 = false;
+                });
+                thread1.detach();
             }
         }
     }
 }
-
 void enableANSI()
 {
     HANDLE ok = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -194,11 +202,10 @@ void enableANSI()
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(ok, dwMode);
 }
-
 int main()
 {
     enableANSI();
-    string path = finder();
+    string path = finder("C:\\");
     clear();
     cout << path;
 
